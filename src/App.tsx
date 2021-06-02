a1f2eb3d107169a19a997156def60cf1ab26ef39
likaiqiang <likaiqiang.frontend@gmail.com>
2021-06-02 21:14:28 +0800

    拆分hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,28 @@
-import React, {ChangeEvent, useCallback, useEffect, useState} from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, {useState,useRef} from 'react';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import {usePaste, useUpload} from "./hooks";
 import './App.css';
-const OSS = require('ali-oss');
-const {
-  REACT_APP_BUCKET,
-  REACT_APP_REGION,
-  REACT_APP_ACCESSKEYSECRET,
-  REACT_APP_ACCESSKEYID
-} = process.env
-const client = new OSS({
-  region: REACT_APP_REGION,
-  accessKeyId: REACT_APP_ACCESSKEYID,
-  accessKeySecret: REACT_APP_ACCESSKEYSECRET,
-  bucket: REACT_APP_BUCKET
-})
 
 function App() {
   const [url,changeUrl] = useState<string>('')
   const [html,changeHtml] = useState<string>('')
   const [md,changeMd] = useState<string>('')
-  const uploadFile = useCallback((e:ChangeEvent<HTMLInputElement>)=>{
-    const files = Array.from(e.target.files || [])
-    for(let file of files){
-      const {name} = file
-      client.put('/images/' + name,file).then(()=>{
-        const url = 'https://likaiqiang-blog.oss-cn-beijing.aliyuncs.com/images/' + name
-        changeUrl(url)
-        changeHtml(`<img src=${url} alt=""/>`)
-        changeMd(`![${name}](${url})`)
-      })
-    }
-  },[])
-  useEffect(()=>{
-    document.addEventListener('paste',(event)=>{
-      const items = event.clipboardData && event.clipboardData.items;
-      let file = null;
-      if (items && items.length) {
-        for (let i = 0; i < items.length; i++) {
-          if (items[i].type.indexOf('image') !== -1) {
-            file = items[i].getAsFile();
-            break;
-          }
-        }
-      }
-      if(file){
-        const name = uuidv4() + '-' + file.name
-        client.put('/images/' + name,file).then(()=>{
-          const url = 'https://likaiqiang-blog.oss-cn-beijing.aliyuncs.com/images/' + name
-          changeUrl(url)
-          changeHtml(`<img alt="" src=${url}/>`)
-          changeMd(`![${name}](${url})`)
-        })
-      }
-    })
-  },[])
+  const updateUrl = useRef((name:string)=>{
+    const url = 'https://likaiqiang-blog.oss-cn-beijing.aliyuncs.com/images/' + name
+    changeUrl(url)
+    changeHtml(`<img src=${url} alt=""/>`)
+    changeMd(`![${name}](${url})`)
+  })
+  const {upload} = useUpload()
+  usePaste(updateUrl.current)
   return (
       <div id="main">
         <section>
           <div className="holder-wrapper">
             <div className="image-holder" title="点击上传图片到微博相册"/>
-            <input type="file" multiple onChange={uploadFile}/>
+            <input type="file" multiple onChange={e=>{
+              upload.current(e,updateUrl.current)
+            }}/>
           </div>
           <div className="table-wrapper">
             <table width="100%">
---
+++ src/hooks/index.ts
@@ -0,0 +1,61 @@
+import {ChangeEvent, useEffect, useRef} from "react";
+import {v4 as uuidv4} from "uuid";
+const OSS = require('ali-oss');
+interface Upload{
+    (e:ChangeEvent<HTMLInputElement>,callback:(name:string)=>void): void
+}
+
+const {
+    REACT_APP_BUCKET,
+    REACT_APP_REGION,
+    REACT_APP_ACCESSKEYSECRET,
+    REACT_APP_ACCESSKEYID
+} = process.env
+
+export const useOss = ()=>{
+    const client = useRef(new OSS({
+        region: REACT_APP_REGION,
+        accessKeyId: REACT_APP_ACCESSKEYID,
+        accessKeySecret: REACT_APP_ACCESSKEYSECRET,
+        bucket: REACT_APP_BUCKET
+    }))
+    const put = useRef((file:File,name:string,callback:(name:string)=>void)=>{
+        client.current.put('/images/' + name,file).then(()=>{
+            callback(name)
+        })
+    })
+    return {client,put}
+}
+
+export const useUpload = ()=>{
+    const {put} = useOss()
+    const upload =  useRef<(Upload)>((e,callback)=>{
+        const files = Array.from(e.target.files || [])
+        for(let file of files){
+            const {name} = file
+            put.current(file,name,callback)
+        }
+    })
+    return {upload}
+}
+export const usePaste = (callback:(name:string)=>void)=>{
+    const {put} = useOss()
+    useEffect(()=>{
+        document.addEventListener('paste',(event)=>{
+            const items = event.clipboardData && event.clipboardData.items;
+            let file = null;
+            if (items && items.length) {
+                for (let i = 0; i < items.length; i++) {
+                    if (items[i].type.indexOf('image') !== -1) {
+                        file = items[i].getAsFile();
+                        break;
+                    }
+                }
+            }
+            if(file){
+                const name = uuidv4() + '-' + file.name
+                put.current(file,name,callback)
+            }
+        })
+    },[put,callback])
+}
